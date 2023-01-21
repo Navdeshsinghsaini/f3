518cc933956fee928640606538cd8ef1951688cf
Navdesh Singh Saini <120104452+Navdeshsinghsaini@users.noreply.github.com>
2023-01-21 22:47:18 +0530

    Add files via upload
    ok

---
+++ login.js
@@ -0,0 +1,13 @@
+import {
+    validateEmailLogin,
+    validatePasswordLogin,
+    validateBoth,
+  } from "./validate.js";
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    validateEmailLogin();
+    validatePasswordLogin();
+    validateBoth();
+  });
